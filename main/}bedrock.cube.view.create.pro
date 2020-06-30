cubewise-code/bedrock

commit 65d4f14044836be32d48f91197ba7b41895a877c

add pSubN (#144)
Add pSubN parameter to processes which call cube.view.create

main/}bedrock.cube.view.create.pro
@@ -4,7 +4,7 @@
 586,
 585,
 564,
-565,"hXUWl:oiavHS?35wWtk_dw[^7f[e?BIDYk4Twu<Wl^aSfY6S^CxHJqmFFZTgC]J:Slubg4rV:RzKk2ZF3NQ>DKYkiyIEbUILxw^7XCDB4dDAKz0]F7MITZgsc6<EhK3W0JWIxGfnu7gcbPKHwop9y7]rbxfg;fb[u:TrUFC1^M<pq53rZp8vlvgkjejZFe9Yqpgsultf"
+565,"tQBNmULn9yWjy<7JpWF0aYqw2N]K?EGPnO]tHpY8SRHOl]wJW]MeCz8?2u9Aw=QGQoZrXWtwNaRR`?1<JYT0wB8p5RGw9BUOq_F@?z=GoX82S2gbbOvL2^Snv`dZxrhR>9BY8DKB9gYExaJDw4<pRDFLL5L]N>WeGw=sHFHe;`s?\P<dOmnbml4aXpe@gXSJ8qtgk8U`"
 559,1
 928,0
 593,
@@ -33,12 +33,12 @@ pFilter
 pSuppressZero
 pSuppressConsol
 pSuppressRules
+pSuppressConsolStrings
 pDimDelim
 pEleStartDelim
 pEleDelim
 pTemp
 pSubN
-pSuppressConsolStrings
 561,13
 1
 2
@@ -47,10 +47,10 @@ pSuppressConsolStrings
 1
 1
 1
-2
-2
-2
 1
+2
+2
+2
 1
 1
 590,13
@@ -61,12 +61,12 @@ pFilter,""
 pSuppressZero,1
 pSuppressConsol,1
 pSuppressRules,1
+pSuppressConsolStrings,0
 pDimDelim,"&"
 pEleStartDelim,"¦"
 pEleDelim,"+"
 pTemp,1
 pSubN,0
-pSuppressConsolStrings,-1
 637,13
 pLogOutput,"OPTIONAL: Write parameters and action summary to server message log (Boolean True = 1)"
 pCube,"REQUIRED: Cube Name"
@@ -75,12 +75,12 @@ pFilter,"OPTIONAL: Filter: Year¦ 2006 + 2007 & Scenario¦ Actual + Budget & Org
 pSuppressZero,"REQUIRED: Suppress Zero Data (Skip = 1)"
 pSuppressConsol,"REQUIRED: Suppress Consolidations (Skip = 1)"
 pSuppressRules,"REQUIRED: Suppress Rules (Skip = 1)"
+pSuppressConsolStrings,"REQUIRED: Suppress Strings on Consolidations (Skip = 1) (Default = 0)"
 pDimDelim,"REQUIRED: Delimiter for start of Dimension/Element set"
 pEleStartDelim,"REQUIRED: Delimiter for start of element list"
 pEleDelim,"REQUIRED: Delimiter between elements"
 pTemp,"OPTIONAL: Make View Temporary (1=Temporary)"
 pSubN,"OPTIONAL: Create N level subset for all dims not mentioned in pFilter"
-pSuppressConsolStrings,"REQUIRED: Suppress Strings on Consolidations (Skip = 1, Include = 0) (Default [Skip] = -1 for backward compatibility)"
 577,0
 578,0
 579,0
@@ -88,15 +88,15 @@ pSuppressConsolStrings,"REQUIRED: Suppress Strings on Consolidations (Skip = 1, 
 581,0
 582,0
 603,0
-572,435
+572,434
 #Region CallThisProcess
 # A snippet of code provided as an example how to call this process should the developer be working on a system without access to an editor with auto-complete.
 If( 1 = 0 );
     ExecuteProcess( '}bedrock.cube.view.create', 'pLogOutput', pLogOutput,
     	'pCube', '', 'pView', '', 'pFilter', '',
-    	'pSuppressZero', 1, 'pSuppressConsol', 1, 'pSuppressRules', 1,
+    	'pSuppressZero', 1, 'pSuppressConsol', 1, 'pSuppressRules', 1, 'pSuppressConsolStrings', 1,
     	'pDimDelim', '&', 'pEleStartDelim', '¦', 'pEleDelim', '+',
-    	'pTemp', 1, 'pSubN', 0, 'pSuppressConsolStrings', 0
+    	'pTemp', 1, 'pSubN', 0
     );
 EndIf;
 #EndRegion CallThisProcess
@@ -523,7 +523,6 @@ If(pSubN = 1);
     End;
 
   EndIf;  
-  
 573,4
 
 #****Begin: Generated Statements***

main/}bedrock.cube.data.clear.pro
@@ -4,7 +4,7 @@
 586,
 585,
 564,
-565,"qZEEu@^wOX>zfr7d9aUJ?clB_TUgdMZtta8Kv8YQq=3C>iuk64?w7kbfkr8p=4hr>rQZwl61GYR>zZjqbY@SgAYT<pKKC<KNj>Z83bI`S:itpa[?w^?`rzSIG_T5^tE__TxBj6;Osg3n?bi\Hx]@rO=]J=CV6D725dhDIy@x`>kBrj=pal=@GI7BbgWCl?_9gX_3i=F8"
+565,"y^H1WI4T3PaTOqOfTjSou6[Ola^?NqW05jURc[<k7I<3qor5Yz`wj2I4`_gQU<ZacxkGH=[yIBvjvFVj7wEwyLFJG9fZ;fyS_jspdHhk@gW>oQ5k4tjarL\lE=xL>?HVdj=2>kiTsrTS>J:B7sWs8Fi:3BX;F7JX=h2Z6^3DlO7iDB^hbBVAf1V0X=F8P069OX906>\K"
 559,1
 928,0
 593,
@@ -25,7 +25,7 @@
 569,0
 592,0
 599,1000
-560,12
+560,13
 pLogOutput
 pCube
 pView
@@ -38,7 +38,8 @@ pEleDelim
 pCubeLogging
 pTemp
 pSandbox
-561,12
+pSubN
+561,13
 1
 2
 2
@@ -51,7 +52,8 @@ pSandbox
 1
 1
 2
-590,12
+1
+590,13
 pLogOutput,0
 pCube,""
 pView,""
@@ -64,7 +66,8 @@ pEleDelim,"+"
 pCubeLogging,0
 pTemp,1
 pSandbox,""
-637,12
+pSubN,0
+637,13
 pLogOutput,"OPTIONAL: Write parameters and action summary to server message log (Boolean True = 1)"
 pCube,"REQUIRED: Cube Name (wildcard * and/or cube1 & cube2 list)"
 pView,"OPTIONAL: View name to be cleared (uses pFilter if pView not specified else clears entire cube)"
@@ -77,6 +80,7 @@ pEleDelim,"OPTIONAL: Delimiter between elements (default value if blank = '+')"
 pCubeLogging,"Required: Cube Logging (0 = No transaction logging, 1 = Logging of transactions, 2 = Ignore Cube Logging - No Action Taken)"
 pTemp,"OPTIONAL: Make Views and subsets Temporary (1=Temporary)"
 pSandbox,"OPTIONAL: To use sandbox not base data enter the sandbox name (invalid name will result in process error)"
+pSubN,"OPTIONAL: Create N level subset for all dims not mentioned in pFilter"
 577,0
 578,0
 579,0
@@ -84,7 +88,7 @@ pSandbox,"OPTIONAL: To use sandbox not base data enter the sandbox name (invalid
 581,0
 582,0
 603,0
-572,470
+572,472
 #Region CallThisProcess
 # A snippet of code provided as an example how to call this process should the developer be working on a system without access to an editor with auto-complete.
 If( 1 = 0 );
@@ -92,7 +96,7 @@ If( 1 = 0 );
     	'pCube', '', 'pView', '', 'pFilter', '',
     	'pFilterParallel', '', 'pParallelThreads', 0,
     	'pDimDelim', '&', 'pEleStartDelim', '¦', 'pEleDelim', '+',
-    	'pCubeLogging', 0, 'pTemp', 1, 'pSandbox', pSandbox
+    	'pCubeLogging', 0, 'pTemp', 1, 'pSandbox', pSandbox, 'pSubN', 0
 	);
 EndIf;
 #EndRegion CallThisProcess
@@ -356,7 +360,8 @@ While( nCubeDelimiterIndex <> 0 );
                     'pDimDelim', pDimDelim,
                     'pEleStartDelim', pEleStartDelim,
                     'pEleDelim', pEleDelim,
-                    'pTemp', pTemp
+                    'pTemp', pTemp,
+                    'pSubN', pSubN
                     );
 
             ### Zero Out View ###
@@ -511,7 +516,8 @@ While( nCubeDelimiterIndex <> 0 );
                   'pDimDelim', pDimDelim,
                   'pEleStartDelim', pEleStartDelim,
                   'pEleDelim', pEleDelim,
-                  'pTemp', pTemp
+                  'pTemp', pTemp,
+                  'pSubN', pSubN
                   );
   
               ### Zero Out View ###

main/}bedrock.cube.data.copy.intercube.pro
@@ -4,7 +4,7 @@
 586,"Bedrock Source Cube"
 585,"Bedrock Source Cube"
 564,
-565,"t@i5cc53IvFPmz_h1Qi2a@S4o]g`t=oI8<1yf9IEnvg\b0q]x3S7XFpbpWG0su@A8ZTinI57bl;^mTOfVslI7JwPvo`T?tPX2R[89\P>pe]IS:wPsE>_yuFLdXFb7I7eqP6o?N`G5uqYM0CzXhxz;ZpcZi^\:80EH5qoYtO[TDa_gR7CU5yb4=\>N\?\Rj@C3Fz=janq"
+565,"jaRzvc4[91ajCHf_`1Bp0xZPC\oWDLq[tJA9zd1?1jCHg3pbh8bkA7e]xNosnS;`pNka;zm5uQSemtj28N<_0SgM@`mX6;2E@znErX7uAXF>kl<iWd3_E5Kf[:PEJFrz1upSXVb\`;T^IWKj>uAz563H>5cu>W9jP=s1jSkxW0J;hJR7XJp?CY1p<U=s9LA^z=Je^wm9"
 559,1
 928,0
 593,
@@ -25,7 +25,7 @@
 569,0
 592,0
 599,1000
-560,20
+560,21
 pLogOutput
 pSrcCube
 pFilter
@@ -45,8 +45,9 @@ pTemp
 pCubeLogging
 pSandbox
 pFile
+pSubN
 pThreadMode
-561,20
+561,21
 1
 2
 2
@@ -67,7 +68,8 @@ pThreadMode
 2
 1
 1
-590,20
+1
+590,21
 pLogOutput,0
 pSrcCube,""
 pFilter,""
@@ -87,8 +89,9 @@ pTemp,1
 pCubeLogging,0
 pSandbox,""
 pFile,0
+pSubN,0
 pThreadMode,0
-637,20
+637,21
 pLogOutput,"OPTIONAL: write parameters and action summary to server message log (Boolean True = 1)"
 pSrcCube,"REQUIRED: Cube data is being copied from"
 pFilter,"OPTIONAL: Filter on source cube in format Year¦ 2006 + 2007 & Scenario¦ Actual + Budget. Blank for whole cube"
@@ -108,6 +111,7 @@ pTemp,"OPTIONAL: Delete temporary view and Subset ( 0 = Retain View and Subsets 
 pCubeLogging,"Required: Cube Logging (0 = No transaction logging, 1 = Logging of transactions, 2 = Ignore Cube Logging - No Action Taken)"
 pSandbox,"OPTIONAL: To use sandbox not base data enter the sandbox name (invalid name will result in process error)"
 pFile,"OPTIONAL: Copy via file export and import. Reduces locks (0 = no, 1= use file and delete it 2= use file and retain it)"
+pSubN,"OPTIONAL: Create N level subset for all dims not mentioned in pFilter"
 pThreadMode,"DO NOT USE: Internal parameter only, please don't use"
 577,29
 V1
@@ -290,7 +294,7 @@ VarType=32ColType=827
 VarType=32ColType=827
 VarType=32ColType=827
 603,0
-572,1350
+572,1351
 #Region CallThisProcess
 # A snippet of code provided as an example how to call this process should the developer be working on a system without access to an editor with auto-complete.
 If( 1 = 0 );
@@ -302,7 +306,7 @@ If( 1 = 0 );
     	'pZeroTarget', 1, 'pZeroSource', 0,
     	'pFactor', 1,
     	'pDimDelim', '&', 'pEleStartDelim', '¦', 'pEleDelim', '+',
-    	'pTemp', 1, 'pCubeLogging', 0, 'pSandbox', pSandbox
+    	'pTemp', 1, 'pCubeLogging', 0, 'pSandbox', pSandbox, 'pSubN', 0
 	);
 EndIf;
 #EndRegion CallThisProcess
@@ -1552,9 +1556,9 @@ Else;
   If( pFile = 0 );
     ### Create View of Source ###
     IF(pSuppressConsol = 0);
-      nSubN=1;
+      pSubN=1;
     else;
-      nSubN=0;
+      pSubN=0;
     Endif;  
     
     nRet = ExecuteProcess('}bedrock.cube.view.create',
@@ -1569,7 +1573,7 @@ Else;
       'pEleStartDelim', pEleStartDelim,
       'pEleDelim', pEleDelim ,
       'pTemp', pTemp,
-      'pSubN', nSubN
+      'pSubN', pSubN
       );
     
     IF(nRet <> 0);
@@ -1582,9 +1586,9 @@ Else;
   ElseIf( pFile > 0 );
     ### Export to File in case of Copy Data Via File ###
     IF(pSuppressConsol = 0);
-      nSubN=1;
+      pSubN=1;
     else;
-      nSubN=0;
+      pSubN=0;
     Endif;  
     
     nRet = ExecuteProcess('}bedrock.cube.data.export',
@@ -1608,7 +1612,8 @@ Else;
        'pDelim', cDelimiter,
        'pQuote', cQuote,
        'pTitleRecord', cTitleRows,
-       'pSandbox', pSandbox
+       'pSandbox', pSandbox,
+       'pSubN', pSubN
       );
     
     IF(nRet <> 0);

main/}bedrock.cube.data.copy.pro
@@ -4,7 +4,7 @@
 586,"zzSYS 50 Dim Cube"
 585,"zzSYS 50 Dim Cube"
 564,
-565,"bAauhBA1pRUyo1aUe[ty3xrE2VrNYUWgp=Nz_\_j=kPeI^q6<qmH4:0\Y@mO6WJTA\b0cVZv=Tww]A>ClAJcu]=3V\Rz=6tbRg4XZM7thySTcd6@ITaBsCS\rtOtje5n;Oa8SKxTA_pqm6pLn[X7GjNi`Oxbk4kTcpNic6U7[JYpmmsz@m5i9Avlh2Tj1NxPx3^YRlBK"
+565,"zPSGLs34LWhiTbOj[WC2VQu_>`aV1HIO1sGV7VY:ReI3fRhALIN7MdaTUMn0_K_mf_`czJ7vbUrTa;zqxs<li>h8Tl6=YK<SeT`;FQKWjvRVIQTwmVXj4f^iP\?8<CIZhiRL8K;RER@^lWWlnSrcUna=b@6RQ:y\aUbpiTG4PWqmMfF7JI[lF24VNBeSGx2NachPfwC9"
 559,1
 928,0
 593,
@@ -25,7 +25,7 @@
 569,0
 592,0
 599,1000
-560,23
+560,24
 pLogOutput
 pCube
 pSrcView
@@ -48,8 +48,9 @@ pTemp
 pCubeLogging
 pSandbox
 pFile
+pSubN
 pThreadMode
-561,23
+561,24
 1
 2
 2
@@ -73,7 +74,8 @@ pThreadMode
 2
 1
 1
-590,23
+1
+590,24
 pLogOutput,0
 pCube,""
 pSrcView,""
@@ -96,8 +98,9 @@ pTemp,1
 pCubeLogging,0
 pSandbox,""
 pFile,0
+pSubN,0
 pThreadMode,0
-637,23
+637,24
 pLogOutput,"OPTIONAL: Write parameters and action summary to server message log (Boolean True = 1)"
 pCube,"REQUIRED: Cube"
 pSrcView,"OPTIONAL: Temporary view name for source"
@@ -120,6 +123,7 @@ pTemp,"OPTIONAL: Delete temporary view and Subset ( 0 = Retain View and Subsets 
 pCubeLogging,"Required: Cube Logging (0 = No transaction logging, 1 = Logging of transactions, 2 = Ignore Cube Logging - No Action Taken)"
 pSandbox,"OPTIONAL: To use sandbox not base data enter the sandbox name (invalid name will result in process error)"
 pFile,"OPTIONAL: Copy via file export and import. Reduces locks (0 = no, 1= use file and delete it 2= use file and retain it)"
+pSubN,"OPTIONAL: Create N level subset for all dims not mentioned in pFilter"
 pThreadMode,"DO NOT USE: Internal parameter only, please don't use"
 577,51
 V1
@@ -434,7 +438,7 @@ VarType=32ColType=827
 VarType=32ColType=827
 VarType=33ColType=827
 603,0
-572,865
+572,864
 #Region CallThisProcess
 # A snippet of code provided as an example how to call this process should the developer be working on a system without access to an editor with auto-complete.
 If( 1 = 0 );
@@ -445,7 +449,7 @@ If( 1 = 0 );
     	'pDimDelim', '&', 'pEleStartDelim', '¦', 'pEleDelim', '+',
     	'pFactor', 1, 'pSuppressConsol', 1, 'pSuppressRules', 1, 'pCumulate', 0,
     	'pZeroTarget', 1, 'pZeroSource', 0,
-    	'pTemp', 1, 'pCubeLogging', 0, 'pSandbox', '', 'pFile', 0
+    	'pTemp', 1, 'pCubeLogging', 0, 'pSandbox', '', 'pFile', 0, 'pSubN', 0
     );
 EndIf;
 #EndRegion CallThisProcess
@@ -722,7 +726,6 @@ sDelimDim           = TRIM( pDimDelim );
 sFilter             = TRIM( pFilter);
 sTargetFilter       = '';
 nSuppressConsol     = pSuppressConsol;
-nSubN               = 0;
 nChar               = 1;
 nCharCount          = LONG( sElementMapping );
 
@@ -921,7 +924,7 @@ If( nCharCount > 0 );
                 If( pSuppressConsol <> 1 );
                   nSuppressConsol = 0;
                 EndIf;
-                nSubN = 1;
+                pSubN = 1;
               Else;
                 sMessage = Expand( 'Target element: %sElement% for dimension %sDimension% is consolidated' );
                 nErrors = nErrors + 1;
@@ -1233,7 +1236,7 @@ Else;
       'pEleStartDelim', pEleStartDelim,
       'pEleDelim', pEleDelim ,
       'pTemp', pTemp,
-      'pSubN', nSubN
+      'pSubN', pSubN
       );
       
     IF(nRet <> 0);

main/}bedrock.cube.data.export.pro
@@ -4,7 +4,7 @@
 586,"}APQ Staging TempSource"
 585,"}APQ Staging TempSource"
 564,
-565,"gXrYhbda^qSfVkI1I5pglecSC:?j4U:]Ljz<T=cGyg;Fl?ZuZg<6mFcCT:j0McgFEzEdqu[BaD=YuLgK3^YKvl7Tarq\nM9JnZR9<aPPZleqRtG=f_>5ZfqiEBoDef_yh39qz2n5]GDb@\ANc@@fu^[LljeO3RDSf0FjXKV8zoz`ghYT5W\BkS8?XS>L1jh>Ce;aJtqC"
+565,"xQfVAyM1Wia8\L\<ZXy@g?f`anYnCi8pw3nlvx2`a5J0\P^wEGcvo0Ccb7gxw=>CNn_phEerw@G]CXXoTV_rdr:6=wb0gk@`_j<5kQyJBANztI\AJ5<z2@;185SAzGZP<bCdklW8dzarCrvQ7]khRZUKC`laTX1v9KZsKhuVvoDFpCbm]l:a59QZa:ape:98WRO^46qG"
 559,1
 928,0
 593,
@@ -25,7 +25,7 @@
 569,0
 592,0
 599,1000
-560,22
+560,23
 pLogoutput
 pCube
 pView
@@ -38,6 +38,7 @@ pEleDelim
 pSuppressZero
 pSuppressConsol
 pSuppressRules
+pSuppressConsolStrings
 pZeroSource
 pCubeLogging
 pTemp
@@ -47,8 +48,8 @@ pDelim
 pQuote
 pTitleRecord
 pSandbox
-pSuppressConsolStrings
-561,22
+pSubN
+561,23
 1
 2
 2
@@ -64,6 +65,7 @@ pSuppressConsolStrings
 1
 1
 1
+1
 2
 2
 2
@@ -71,7 +73,7 @@ pSuppressConsolStrings
 1
 2
 1
-590,22
+590,23
 pLogoutput,0
 pCube,""
 pView,""
@@ -84,6 +86,7 @@ pEleDelim,"+"
 pSuppressZero,1
 pSuppressConsol,1
 pSuppressRules,1
+pSuppressConsolStrings,0
 pZeroSource,0
 pCubeLogging,0
 pTemp,1
@@ -93,8 +96,8 @@ pDelim,","
 pQuote,""""
 pTitleRecord,1
 pSandbox,""
-pSuppressConsolStrings,0
-637,22
+pSubN,0
+637,23
 pLogoutput,"OPTIONAL: Write parameters and action summary to server message log (Boolean True = 1)"
 pCube,"REQUIRED: Cube name"
 pView,"OPTIONAL: Temporary view name"
@@ -107,6 +110,7 @@ pEleDelim,"OPTIONAL: Delimiter between elements  (default value if blank = '+')"
 pSuppressZero,"OPTIONAL: Suppress Zero Values (1=Suppress)"
 pSuppressConsol,"OPTIONAL: Suppress Consolidated Values? (1=Suppress)"
 pSuppressRules,"OPTIONAL: Suppress Rule Values? (1=Suppress)"
+pSuppressConsolStrings,"OPTIONAL: Suppress Strings on Consolidations (Skip = 1) (Default = 0)"
 pZeroSource,"OPTIONAL: Zero Out view AFTER Copy? (Boolean 1=True)"
 pCubeLogging,"Required: Cube Logging (0 = No transaction logging, 1 = Logging of transactions, 2 = Ignore Cube Logging - No Action Taken)"
 pTemp,"OPTIONAL: Retain temporary view and Subset ( 0 = retain View and Subsets 1 = use temp objects)"
@@ -116,7 +120,7 @@ pDelim,"OPTIONAL: AsciiOutput delimiter character (Default=comma, exactly 3 digi
 pQuote,"OPTIONAL: AsciiOutput quote character (Accepts empty quote, exactly 3 digits = ASCII code)"
 pTitleRecord,"OPTIONAL: Include Title Record in Export File? (Boolean 0=false, 1=true, 2=title and filter line Default=1)"
 pSandbox,"OPTIONAL: To use sandbox not base data enter the sandbox name (invalid name will result in process error)"
-pSuppressConsolStrings,"OPTIONAL: Suppress Strings on Consolidations (Skip = 1) (Default = 0)"
+pSubN,"OPTIONAL: Create N level subset for all dims not mentioned in pFilter"
 577,101
 V1
 V2
@@ -730,7 +734,7 @@ VarType=32ColType=827
 VarType=32ColType=827
 VarType=32ColType=827
 603,0
-572,363
+572,364
 #Region CallThisProcess
 # A snippet of code provided as an example how to call this process should the developer be working on a system without access to an editor with auto-complete.
 If( 1 = 0 );
@@ -738,10 +742,10 @@ If( 1 = 0 );
     	'pCube', '', 'pView', '', 'pFilter', '',
     	'pFilterParallel', '', 'pParallelThreads', 0,
     	'pDimDelim', '&', 'pEleStartDelim', '¦', 'pEleDelim', '+',
-    	'pSuppressZero', 1, 'pSuppressConsol', 1, 'pSuppressRules', 1,
+    	'pSuppressZero', 1, 'pSuppressConsol', 1, 'pSuppressRules', 1, 'pSuppressConsolStrings', 1,
     	'pZeroSource', 0, 'pCubeLogging', 0, 'pTemp', 1,
     	'pFilePath', '', 'pFileName', '',
-    	'pDelim', ',', 'pQuote', '"', 'pTitleRecord', 1, 'pSandbox', pSandbox
+    	'pDelim', ',', 'pQuote', '"', 'pTitleRecord', 1, 'pSandbox', pSandbox, 'pSubN', pSubN
 	);
 EndIf;
 #EndRegion CallThisProcess
@@ -1066,11 +1070,12 @@ Else;
           'pSuppressZero', pSuppressZero,
           'pSuppressConsol', pSuppressConsol,
           'pSuppressRules', pSuppressRules,
+          'pSuppressConsolStrings', pSuppressConsolStrings,
           'pDimDelim', pDimDelim,
           'pEleStartDelim', pEleStartDelim,
-          'pEleDelim', pEleDelim ,
+          'pEleDelim', pEleDelim,
           'pTemp', pTemp,
-          'pSuppressConsolStrings', pSuppressConsolStrings
+          'pSubN', pSubN
           );
   
   IF( nRet <> ProcessExitNormal() );

commit 20ee926f25c2af076105aa66b35ab1884b717cf3

Fix default value for pSupressConsolStrings in view.create (#185)
* Fix pSupressConsolStrings default value
Backwards compatibility was broken in #144. Restoring correct default parameter value

main/}bedrock.cube.view.create.pro
@@ -61,7 +61,7 @@ pFilter,""
 pSuppressZero,1
 pSuppressConsol,1
 pSuppressRules,1
-pSuppressConsolStrings,0
+pSuppressConsolStrings,-1
 pDimDelim,"&"
 pEleStartDelim,"¦"
 pEleDelim,"+"
@@ -75,7 +75,7 @@ pFilter,"OPTIONAL: Filter: Year¦ 2006 + 2007 & Scenario¦ Actual + Budget & Org
 pSuppressZero,"REQUIRED: Suppress Zero Data (Skip = 1)"
 pSuppressConsol,"REQUIRED: Suppress Consolidations (Skip = 1)"
 pSuppressRules,"REQUIRED: Suppress Rules (Skip = 1)"
-pSuppressConsolStrings,"REQUIRED: Suppress Strings on Consolidations (Skip = 1) (Default = 0)"
+pSuppressConsolStrings,"REQUIRED: Suppress Strings on Consolidations (Skip = 1, Include = 0) (Default [Skip] = -1 for backward compatibility)"
 pDimDelim,"REQUIRED: Delimiter for start of Dimension/Element set"
 pEleStartDelim,"REQUIRED: Delimiter for start of element list"
 pEleDelim,"REQUIRED: Delimiter between elements"